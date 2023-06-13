37b470728f7aaee250bfceb8d7b8580d01b3734a
masudrana-1 <masudrana58855@gmail.com>
2023-06-13 12:21:09 +0600

    pagination

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,5 +1,16 @@
 import { IGenericErrorMessage } from './error';
 
+// pagination type
+
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+};
+
 // error handler interface
 export type IGenericErrorResponse = {
   statusCode: number;
